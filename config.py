0b905fc6e05c8b344109bcea0387861944ebecca
kajjidonisuneet <kajjidonisuneet@gmail.com>
2022-07-11 13:45:28 +0530

    heroku dep changes

--- config.py
+++ config.py
@@ -7,8 +7,7 @@ load_dotenv(os.path.join(basedir,'.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://','postgresql://') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
@@ -20,5 +19,6 @@ class Config(object):
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     #SQLALCHEMY_ECHO = True
 
