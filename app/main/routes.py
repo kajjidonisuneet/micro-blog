46349294984e398ff7f1a6114d389a4be614e565
kajjidonisuneet <kajjidonisuneet@gmail.com>
2022-07-19 05:50:44 +0530

    for heroku dep disable search and background process

--- app/main/routes.py
+++ app/main/routes.py
@@ -188,6 +188,7 @@ def export_posts():
     if current_user.get_task_in_progress('export_posts'):
         flash(_('An export task is currently in progress'))
     else:
-        current_user.launch_task('export_posts',_('Exporting posts ...'))
-        db.session.commit()
+        #current_user.launch_task('export_posts',_('Exporting posts ...'))
+        #db.session.commit()
+        flash(_('Export posts feature is not availabe on heroku'))
     return redirect(url_for('main.user', username=current_user.username))
